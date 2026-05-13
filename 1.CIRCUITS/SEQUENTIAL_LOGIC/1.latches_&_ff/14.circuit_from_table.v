92caa6cdfd379180b031309d80b953547623ea55
22ece45rangarajan <22ece45rangarajan@gmail.com>
2026-05-13 09:29:13 +0530

    Create 14.circuit_from_table.v

---
+++ 1.CIRCUITS/SEQUENTIAL_LOGIC/1.latches_&_ff/14.circuit_from_table.v
@@ -0,0 +1,15 @@
+module top_module (
+    input clk,
+    input j,
+    input k,
+    output reg Q); 
+    always@(posedge clk)begin
+        case({j,k})
+            0:Q=Q;
+            1:Q=0;
+            2:Q=1;
+            3:Q=~Q;
+            default:Q=0;
+        endcase
+    end
+endmodule
