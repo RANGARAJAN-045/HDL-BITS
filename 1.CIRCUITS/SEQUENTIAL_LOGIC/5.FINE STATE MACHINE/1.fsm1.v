d7c7836229bc51a61aa445b1f9281cbe609ad47e
RANGARAJAN_M <22ece45rangarajan@gmail.com>
2026-05-23 11:33:41 +0530

    Create 1.fsm1.v

---
+++ 1.CIRCUITS/SEQUENTIAL_LOGIC/5.FINE STATE MACHINE/1.fsm1.v
@@ -0,0 +1,29 @@
+module top_module(
+    input clk,
+    input areset,    // Asynchronous reset to state B
+    input in,
+    output out);//  
+
+    parameter A=0, B=1; 
+    reg state, next;
+
+    always @(*) begin    
+        case (state)
+            B: next = in ? B : A;
+            A: next = in ? A : B;
+            default: next = B;
+        endcase
+    end
+
+    always @(posedge clk, posedge areset) begin    
+        if(areset) begin
+            state <= B;
+        end
+        else begin
+            state <= next;
+        end
+    end
+
+    assign out = (state == B) ? 1: 0;
+
+endmodule
